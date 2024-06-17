5a3d2843aa7128f01055a8e8cfda40eb5d9f2562
sjhmichael <65953182+sjhmichael@users.noreply.github.com>
2024-06-18 02:34:58 +0800

    update

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -15,11 +15,13 @@ function MoviePage() {
   const [companies, setCompanies] = useState([]);
   const [videos, setVideos] = useState({});
   const [credits, setCredits] = useState({});
+  const [language, setLanguage] = useState([]);
+  const [similar, setSimilar] = useState([]);
 
   useEffect(() => {
     axios
       .get(
-        `https://api.themoviedb.org/3/movie/${id}?api_key=${requests.key}&append_to_response=videos,images,credits`,
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${requests.key}&append_to_response=videos,images,credits,similar`,
       )
       .then((response) => {
         setMovieDetails(response.data);
@@ -27,6 +29,8 @@ function MoviePage() {
         setCompanies(response.data.production_companies);
         setVideos(response.data.videos.results);
         setCredits(response.data.credits.cast);
+        setLanguage(response.data.spoken_languages[0].english_name);
+        setSimilar(response.data.similar.results);
       });
   }, [id, movie]);
 
@@ -49,7 +53,6 @@ function MoviePage() {
   };
 
   console.log("moviedetails: ", movieDetails);
-  console.log("credits details", credits);
 
   return (
     <div className="relative w-full text-white">
@@ -68,35 +71,39 @@ function MoviePage() {
 
       <div className="absolute top-[250px] z-20 h-full w-full p-4 md:p-8">
         <div className="relative left-0 right-0 mx-auto max-w-[1400px]">
-          <h1 className="my-8 text-3xl font-medium md:text-5xl">
-            {movieDetails.title}
-          </h1>
-          <div className="flex flex-row space-x-2">
-            {" "}
-            <p className="mb-6 text-sm text-gray-300">
-              {arraySlice(movieDetails?.release_date, 4)} |
+          {/* Overview section */}
+          <div className="min-h-[250px]">
+            <h1 className="my-8 text-3xl font-medium md:text-5xl">
+              {movieDetails.title}
+            </h1>
+            <div className="flex flex-row space-x-2">
+              {" "}
+              <p className="mb-6 text-sm text-gray-300">
+                {arraySlice(movieDetails?.release_date, 4)} |
+              </p>
+              <p className="mb-6 text-sm text-gray-300">
+                {movieDetails?.runtime} Minutes
+              </p>
+            </div>
+            <p className="text-balance text-sm text-gray-300 md:text-base">
+              {movieDetails?.overview}
             </p>
-            <p className="mb-6 text-sm text-gray-300">
-              {movieDetails?.runtime} Minutes
-            </p>
-          </div>
-          <p className="text-balance text-sm text-gray-300 md:text-base">
-            {movieDetails?.overview}
-          </p>
 
-          {/* buttons */}
-          <div className="my-6 flex flex-row items-center">
-            <button className="rounded-full border bg-white px-8 py-3 text-xl text-black">
-              <div className="flex flex-row items-center gap-x-4 font-medium">
-                <FaPlay />
-                Play
-              </div>
-            </button>
+            {/* buttons */}
+            <div className="my-6 flex flex-row items-center">
+              <button className="rounded-full border bg-white px-8 py-3 text-xl text-black">
+                <div className="flex flex-row items-center gap-x-4 font-medium">
+                  <FaPlay />
+                  Play
+                </div>
+              </button>
+            </div>
           </div>
 
-          <div className="absolute mt-[50px] flex w-full flex-col text-white lg:mt-[200px]">
+          {/* Details Section */}
+          <div className="mt-[50px] flex w-full flex-col text-white lg:mt-[200px]">
             <h1 className="text-3xl font-medium">More Details</h1>
-            <div className="mt-8 flex flex-row md:space-x-8">
+            <div className="mt-4 flex flex-row md:space-x-8">
               <img
                 className="relative hidden size-1/6 md:block"
                 src={`https://image.tmdb.org/t/p/original/${movieDetails?.poster_path}`}
@@ -120,59 +127,75 @@ function MoviePage() {
                   </div>
                   <div className="flex flex-col space-y-2">
                     <h1 className="text-sm text-gray-300">Language</h1>
-                    <p className="">{movieDetails?.original_language}</p>
+                    <p className="">{language}</p>
                   </div>
                 </div>
                 <div className="mt-8 flex flex-col space-y-2">
                   <h1 className="text-sm text-gray-300">Cast</h1>
-                  {credits.length > 0 && (
-                    <p>
-                      {arraySlice(
-                        credits.map((credits) => credits.name).join(", "),
-                        200,
-                      ) + "..."}
-                    </p>
-                  )}
+                  <p>
+                    {credits.length > 0 &&
+                      arraySlice(
+                        credits.map((credits) => credits.name),
+                        20,
+                      ).join(", ")}
+                  </p>
                 </div>
                 <div className="mt-8 flex flex-col space-y-2">
                   <h1 className="text-sm text-gray-300">Homepage</h1>
-                  <p className="">
-                    {movieDetails?.homepage ? movieDetails?.homepage : "-"}
-                  </p>
+                  <p>{movieDetails?.homepage ? movieDetails?.homepage : "-"}</p>
                 </div>
               </div>
             </div>
 
+            {/* Videos section */}
+
             <div className="w-full">
-              <div>
-                <h2 className="my-8 text-3xl font-medium">Videos</h2>
-                <div className="group relative flex items-center">
-                  {videos.length > 0 && (
-                    <div
-                      id={"slider"}
-                      className="relative flex h-full w-full flex-row overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide"
-                    >
-                      {arraySlice(
-                        videos.map(
-                          (video) =>
-                            video.site === "YouTube" && (
-                              <iframe
-                                key={video.id}
-                                width="640"
-                                height="200"
-                                src={`https://www.youtube.com/embed/${video.key}`}
-                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                                title={video.name}
-                                className="h-[315px] w-full max-w-[560px] pr-8 lg:h-[315px] lg:w-[560px]"
-                              ></iframe>
-                            ),
-                        ),
-                        3,
-                      )}
-                    </div>
-                  )}
-                </div>
+              <h2 className="mb-4 mt-16 text-3xl font-medium">
+                Videos:{" "}
+                <span className="font-normal text-gray-400">
+                  {movieDetails?.title}
+                </span>
+              </h2>
+              <div className="group relative flex items-center">
+                {videos.length > 0 && (
+                  <div
+                    id={"slider"}
+                    className="relative flex h-full w-full flex-row overflow-x-scroll scroll-smooth whitespace-nowrap scrollbar-hide"
+                  >
+                    {arraySlice(
+                      videos.map(
+                        (video) =>
+                          video.site === "YouTube" && (
+                            <iframe
+                              key={video.id}
+                              width="640"
+                              height="200"
+                              src={`https://www.youtube.com/embed/${video.key}`}
+                              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                              allowFullScreen
+                              title={video.name}
+                              className="h-[315px] w-full max-w-[560px] pr-8 lg:h-[315px] lg:w-[560px]"
+                            ></iframe>
+                          ),
+                      ),
+                      3,
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+
+            {/* Similar */}
+            <div className="w-full">
+              <h2 className="mb-4 mt-16 text-3xl font-medium">Similar</h2>
+              <div className="grid grid-cols-[auto_auto] flex-wrap gap-4 md:grid-cols-[auto_auto_auto_auto]">
+                {similar.map((similar) => (
+                  <img
+                    className="max-h-[196px] w-full rounded-lg object-cover"
+                    src={`https://image.tmdb.org/t/p/w500/${similar.poster_path}`}
+                    alt={similar.original_title}
+                  />
+                ))}
               </div>
             </div>
           </div>
