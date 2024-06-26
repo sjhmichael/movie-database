01e69f1175e49caf64e8331dbfc67062a7245b4b
sjhmichael <65953182+sjhmichael@users.noreply.github.com>
2024-06-27 04:08:06 +0800

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,10 +6,15 @@ import { FaPlay, FaArrowRight } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 import PlayButton from "./PlayButton";
 import Truncate from "./Truncate";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 function Main() {
   const [movies, setMovies] = useState([]);
   const navigate = useNavigate();
+  const container = useRef(null);
+  gsap.registerPlugin(useGSAP);
 
   useEffect(() => {
     axios.get(requests.requestPopular).then((e) => {
@@ -32,17 +37,41 @@ function Main() {
     navigate(`/movie/${movie.id}`, { state: { movie } });
   };
 
+  useGSAP(() => {
+    const tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+
+    tl.fromTo(
+      ".hero__image",
+      { opacity: 0, scale: 0.8 },
+      { scale: 1, opacity: 1, duration: 1 },
+    );
+
+    tl.fromTo(
+      ".movie__details",
+      { y: 20 },
+      { y: 0, opacity: 1, duration: 1 },
+      "-0.2",
+    );
+
+    tl.fromTo(
+      ".movie__buttons",
+      { x: -20 },
+      { x: 0, opacity: 1, duration: 1 },
+      "+0.2",
+    );
+  });
+
   return (
-    <div className="h-[650px] w-full text-white">
+    <div className="h-[650px] w-full text-white" ref={container}>
       <div className="h-full w-full">
-        <div className="absolute h-[650px] w-full bg-gradient-to-t from-black"></div>
+        <div className="absolute z-10 h-[650px] w-full bg-gradient-to-t from-black" />
         {/* movie?.title is for optional chanining */}
         <img
-          className="h-full w-full object-cover"
+          className="hero__image h-full w-full object-cover"
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         ></img>
-        <div className="absolute top-[250px] w-full p-4 md:p-8">
+        <div className="movie__details absolute top-[250px] z-10 w-full p-4 opacity-0 md:p-8">
           <div className="flex flex-row space-x-4">
             <h1 className="w-fit rounded-full bg-gray-400/30 px-4 py-2 text-white">
               Popular
@@ -58,7 +87,7 @@ function Main() {
             {truncateString(movie?.overview, 200)}
           </p>
           {/* buttons */}
-          <div className="my-6 flex flex-row items-center">
+          <div className="movie__buttons my-6 flex flex-row items-center">
             <PlayButton label={"Play"} />
             <button
               className="group ml-4 rounded-full border border-gray-600 px-8 py-3 text-xl text-white"
